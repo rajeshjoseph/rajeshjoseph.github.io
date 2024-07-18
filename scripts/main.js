bd1ee00eab827574c06566c095b97acdfb927931
Rajesh Joseph <joseph.rajesh@andromeda.com>
2024-07-18 22:33:31 +0530

    white spaces fixed

--- scripts/main.js
+++ scripts/main.js
@@ -5,63 +5,59 @@ let sections = document.querySelectorAll("section"),
   headings = gsap.utils.toArray(".section-heading"),
   outerWrappers = gsap.utils.toArray(".outer"),
   innerWrappers = gsap.utils.toArray(".inner"),
-  //splitHeadings = headings.map(heading => new SplitText(heading, { type: "chars,words,lines", linesClass: "clip-text" })),
   currentIndex = -1,
-  wrap = gsap.utils.wrap(0, sections.length),
   animating;
 
 gsap.set(outerWrappers, { yPercent: 100 });
 gsap.set(innerWrappers, { yPercent: -100 });
 
 function gotoSection(index, direction) {
-  //index = wrap(index); // make sure it's valid
-  if (index >= sections.length) {
-	  return
-  }
-  
-  if (currentIndex == 0 && direction == -1 ) {
-	  return
-  }
+    if (index >= sections.length) {
+        return
+    }
 
-  
-  animating = true;
-  let fromTop = direction === -1,
-      dFactor = fromTop ? -1 : 1,
-      tl = gsap.timeline({
-        defaults: { duration: 1.25, ease: "power1.inOut" },
-        onComplete: () => animating = false
-      });
-  if (currentIndex >= 0) {
-    // The first time this function runs, current is -1
-    gsap.set(sections[currentIndex], { zIndex: 0 });
-    tl.to(images[currentIndex], { yPercent: -15 * dFactor })
-      .set(sections[currentIndex], { autoAlpha: 0 });
-  }
-  gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
-  tl.fromTo([outerWrappers[index], innerWrappers[index]], { 
-      yPercent: i => i ? -100 * dFactor : 100 * dFactor
-    }, { 
-      yPercent: 0 
+    if (currentIndex == 0 && direction == -1 ) {
+        return
+    }
+
+    animating = true;
+    let fromTop = direction === -1,
+        dFactor = fromTop ? -1 : 1,
+        tl = gsap.timeline({
+            defaults: { duration: 1.25, ease: "power1.inOut" },
+            onComplete: () => animating = false
+        });
+    if (currentIndex >= 0) {
+        // The first time this function runs, current is -1
+        gsap.set(sections[currentIndex], { zIndex: 0 });
+        tl.to(images[currentIndex], { yPercent: -15 * dFactor })
+            .set(sections[currentIndex], { autoAlpha: 0 });
+    }
+    gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
+    tl.fromTo([outerWrappers[index], innerWrappers[index]], {
+        yPercent: i => i ? -100 * dFactor : 100 * dFactor
+    }, {
+        yPercent: 0
     }, 0)
-    .fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0)
-   
+        .fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0)
 
-  currentIndex = index;
+
+    currentIndex = index;
 }
 
 Observer.create({
-  type: "wheel,touch,pointer",
-  wheelSpeed: -1,
-  onDown: () => !animating && gotoSection(currentIndex - 1, -1),
-  onUp: () => !animating && gotoSection(currentIndex + 1, 1),
-  tolerance: 5,
-  preventDefault: true
+    type: "wheel,touch,pointer",
+    wheelSpeed: -1,
+    onDown: () => !animating && gotoSection(currentIndex - 1, -1),
+    onUp: () => !animating && gotoSection(currentIndex + 1, 1),
+    tolerance: 5,
+    preventDefault: true
 });
 
 gotoSection(0, 1);
 
 function scrollToSection() {
-	gotoSection(3, 1);
+    gotoSection(3, 1);
 }
 
 
@@ -73,33 +69,33 @@ const contact = document.querySelector(".contact");
 var tl = gsap.timeline({ paused: true });
 
 tl.to(menu, {
-	duration: 1,
-	opacity: 1,
-	height: '30vh', // change this to 100vh for full-height menu
-	ease: 'expo.inOut',
+    duration: 1,
+    opacity: 1,
+    height: '30vh', // change this to 100vh for full-height menu
+    ease: 'expo.inOut',
 })
 tl.from(links, {
-	duration: 1,
-	opacity: 0,
-	y: 10,
-	stagger: 0.1,
-	ease: 'expo.inOut',
+    duration: 1,
+    opacity: 0,
+    y: 10,
+    stagger: 0.1,
+    ease: 'expo.inOut',
 }, "-=0.5");
 
 tl.reverse();
 
 ham.addEventListener('click', () => {
-	tl.reversed(!tl.reversed());
+    tl.reversed(!tl.reversed());
 });
 
 document.addEventListener('click', (event) => {
-	if (!ham.contains(event.target)) {
-		tl.reversed(true);
-	}
-	
-	if (contact.contains(event.target)) {
-		scrollToSection()
-	}
+    if (!ham.contains(event.target)) {
+        tl.reversed(true);
+    }
+
+    if (contact.contains(event.target)) {
+        scrollToSection()
+    }
 });
 
 // original: https://codepen.io/BrianCross/pen/PoWapLP
